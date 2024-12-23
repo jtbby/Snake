bb1f0287c65f5c57785ff5d0d97eafedf631e6f3
jtbby <justin.marte@hotmail.com>
2024-12-23 13:05:53 -0500

    expand snake logic done

--- game/logic.py
+++ game/logic.py
@@ -12,6 +12,11 @@ import random
 # 0 is unoccupied
 # 1 is snake
 # 2 is food
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.next = None
 
 
 #  NOTE ONCE THE SNAKE HAS EATEN THE APPLE, KEEP THE TAIL IN THE SAME SPOT
@@ -20,40 +25,38 @@ class gameBoard:
         self.board = [[0] * 20 for i in range(20)]
         self.direction = 'r'
         self.board[9][5] = 1  # Start the snake in the middle of the board
-        self.snakeHead = (9, 5)
-        self.snakeTail = (9, 5)
+        self.snakeHead = Node(9, 5)
+        self.snakeTail = self.snakeHead
         self.lastMovement = time.time()
         self.placeApple()
 
     # Automatically move snake if no key pressed within .5s
     # Move otherwise
     def moveSnake(self):
-        x, y = self.snakeHead
-        tailX, tailY = self.snakeTail
+        x, y = self.snakeHead.x, self.snakeHead.y
         ate = False
         if self.direction == 'r':
-            self.snakeHead = (x, y + 1)
             ate = self.checkEating(x, y + 1)
-            self.board[x][y + 1] = 1
+            newX, newY = x, y + 1
 
         elif self.direction == 'l':
-            self.snakeHead = (x, y - 1)
             ate = self.checkEating(x, y - 1)
-            self.board[x][y - 1] = 1
+            newX, newY = x, y - 1
 
         elif self.direction == 'u':
-            self.snakeHead = (x - 1, y)
             ate = self.checkEating(x - 1, y)
-            self.board[x - 1][y] = 1
+            newX, newY = x - 1, y
 
         elif self.direction == 'd':
-            self.snakeHead = (x + 1, y)
             ate = self.checkEating(x + 1, y)
-            self.board[x + 1][y] = 1
+            newX, newY = x + 1, y
 
-        if not ate:
-            self.board[tailX][tailY] = 0
-            self.snakeTail = ()
+        self.board[newX][newY] = 1
+        self.snakeHead.next = Node(newX, newY)
+        self.snakeHead = self.snakeHead.next
+
+        if not ate: # move the snake
+            self.moveSnakeBody()
 
         self.lastMovement = time.time()
 
@@ -81,6 +84,13 @@ class gameBoard:
             self.placeApple()
             return True
         return False
+
+    def moveSnakeBody(self):
+        old = self.snakeTail
+        self.board[old.x][old.y] = 0
+        self.snakeTail = self.snakeTail.next
+        old.next = None
+        curr = self.snakeTail
 
 
 def listen(game):
